3c13b54800f44c2a5451507599d747a589879265
felipe broch lunardon <felipebrochlunardon@pop-os.localdomain>
2022-02-26 18:20:23 -0300

    refactor de casos 4 e 9  para os valores romanos IV e IX respectivamente

--- src/UnitConverter.php
+++ src/UnitConverter.php
@@ -6,6 +6,13 @@ class UnitConverter
 {
     public static function unitConverter(int $amount): string
     {
+        if ($amount == 4) {
+            return 'IV';
+        }
+        if ($amount == 9) {
+            return 'IX';
+        }
+
         if ($amount >= 5) {
             return sprintf('V%s',str_repeat('I', $amount-5));
         }
